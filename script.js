f575d152960e2c400e90dcff959d2ab975b9e20c
krishna9809 <multiatoms980996@gmail.com>
2024-02-12 18:15:39 +0530

    page-4

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
+  el: document.querySelector(".main"),
+  smooth: true,
 });
+
+var elemC = document.querySelector(".elem-container");
+var fixed = document.querySelector(".fixed-img");
+elemC.addEventListener("mouseenter", function () {
+  fixed.style.display = "block";
+});
+
+elemC.addEventListener("mouseleave", function () {
+  fixed.style.display = "none";
+});
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var image = e.getAttribute("data-image");
+    fixed.style.backgroundImage = `url(${image})`;
+  });
+});
+
+var a = document.querySelectorAll(".head-text");
+var b = document.querySelector(".page-4-right")
+a.forEach(function(image){
+image.addEventListener("click", function(){
+  var page4Image = image.getAttribute("data-image");
+  b.style.backgroundImage = `url(${page4Image})`;
+})
+})
+
+
+
